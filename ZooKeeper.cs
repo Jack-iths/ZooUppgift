d04f3d781c06a5021113cd5ba2fd3d83c94a7eea
Jack Reveny <Jack.reveny@iths.se>
2025-09-19 12:56:40 +0200

    ctor i ZooKeeper.cs

--- ZooKeeper.cs
+++ ZooKeeper.cs
@@ -4,9 +4,14 @@ public class ZooKeeper
 {
     public string Name { get; private set; }
 
+    public ZooKeeper(string name)
+    {
+        Name = name;
+    }
+
     public void FeedAnimal()
     {
-        
+
     }
     
 }
